ae0116dcffcc1c77fde3f45956c2cbae267d47d0
sandrim01 <84296608+sandrim01@users.noreply.github.com>
2025-06-19 22:01:08 -0300

    Criação automática do usuário admin Alessandro_jr no script de migração

--- migrate_db.py
+++ migrate_db.py
@@ -4,3 +4,19 @@ import models
 with app.app_context():
     db.create_all()
     print('Tabelas criadas com sucesso!')
+
+    # Criação do usuário administrador Alessandro_jr
+    if not models.User.query.filter_by(username="Alessandro_jr").first():
+        user = models.User(
+            username="Alessandro_jr",
+            name="Alessandro Junior",
+            email="alessandro_jr@example.com",
+            role=models.UserRole.admin,
+            active=True
+        )
+        user.set_password("Zx1205698979*#")
+        db.session.add(user)
+        db.session.commit()
+        print("Usuário administrador Alessandro_jr criado com sucesso!")
+    else:
+        print("Usuário Alessandro_jr já existe.")
